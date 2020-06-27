989407548f9bd4efed96fefc41d2cdf546c11d82
unknown <Katherine2bk@gmail.com>
2020-06-27 10:03:48 -0700

    set up axios call

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const FETCHING_SMURF_START = "FETCHING_SMURF_START";
+export const FETCHING_SMURF_SUCCESS = "FETCHING_SMURF_SUCCESS";
+export const FETCHING_SMURF_FAILURE = "FETCHING_SMURF_FAILURE";
+
+export const POST_SMURF_START = "POST_SMURF_START";
+export const POST_SMURF_SUCCESS = "POST_SMURF_SUCCESS";
+export const POST_SMURF_FAILURE = "POST_SMURF_FAILURE";
+
+// we need to get the data, create a function dispatch, and we need to add the data, create a function dispatch for that too
+
+export const getSmurfs = () = (dispatch) => {
+    dispatch({type: FETCHING_SMURF_START});
+    axios.get("https://localhost:3333/smurfs")
+    .then((response) => {
+        console.log(response);
+    })
+}
